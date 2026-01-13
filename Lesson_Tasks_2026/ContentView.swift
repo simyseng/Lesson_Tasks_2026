1e4b1cc74c3a276bc8193dcc80eb55ffe5184f93
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-01-13 11:01:22 +0800

    Update ContentView.swift

--- Lesson_Tasks_2026/ContentView.swift
+++ Lesson_Tasks_2026/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var taskTitles: [String] = [
+        "Finish Maths Homework",
+        "Pack PE Attire",
+        "Practise Piano",
+        "Read 20 Pages of Book"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(taskTitles, id: \.self) { title in
+                    Text(title)
+                }
+            }.navigationTitle("Tasks")
         }
-        .padding()
     }
 }
 
