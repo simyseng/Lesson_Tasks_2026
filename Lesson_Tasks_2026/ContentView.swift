8a84b8bc2b3f932ef91da19b538bf6de65f4ad92
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-01-15 15:07:41 +0800

    Squashed commit of the following:
    commit fb1f635b00ca6fd7025e2c5ec898cad197ad695c
    Author: Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
    Date:   Thu Jan 15 15:01:02 2026 +0800
    
        Update ContentView.swift

--- Lesson_Tasks_2026/ContentView.swift
+++ Lesson_Tasks_2026/ContentView.swift
@@ -1,1 +1,24 @@
+import SwiftUI
 
+struct ContentView: View {
+    @State private var taskTitles: [String] = [
+        "Maths Homework",
+        "Pack PE Attire",
+        "Practise guitar"
+    ]
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(taskTitles, id: \.self) { task in
+                    Text(task)
+                }
+            }
+            .navigationTitle("Tasks")
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
