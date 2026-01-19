76165f83e5e741efd79c43efb3f0fb32de2e02ad
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-01-19 14:03:10 +0800

    Commit

--- Lesson_Tasks_2026/ContentView.swift
+++ Lesson_Tasks_2026/ContentView.swift
@@ -2,24 +2,30 @@
 //  ContentView.swift
 //  Lesson_Tasks_2026
 //
-//  Created by Stanley Sim on 8/1/26.
+//  Created by Stanley Sim on 19/1/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var taskTitles: [String] = [
-        "Finish Maths Homework",
-        "Pack PE Attire",
-        "Practise Piano",
-        "Read 20 Pages of Book"
+    @State private var tasks: [Task] = [
+        Task(title:"Finish Maths Homework", category: "School"),
+        Task(title:"Pack PE Attire", category: "School"),
+        Task(title:"Practise Piano", category: "Personal"),
     ]
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(taskTitles, id: \.self) { title in
-                    Text(title)
+                ForEach(tasks) { task in
+                    VStack(alignment: .leading) {
+                        Text(task.title)
+                            .font(.headline)
+                        Text(task.category)
+                            .font(.caption)
+                            .foregroundStyle(Color.secondary)
+                    }
+                    
                 }
             }.navigationTitle("Tasks")
         }
