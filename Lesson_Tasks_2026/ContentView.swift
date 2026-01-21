65c2831a2ae1a6fc6570ad279ea31884350e03ae
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-01-21 10:01:42 +0800

    Commit

--- Lesson_Tasks_2026/ContentView.swift
+++ Lesson_Tasks_2026/ContentView.swift
@@ -18,14 +18,15 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach(tasks) { task in
-                    VStack(alignment: .leading) {
-                        Text(task.title)
-                            .font(.headline)
-                        Text(task.category)
-                            .font(.caption)
-                            .foregroundStyle(Color.secondary)
+                    NavigationLink(destination: TaskDetailView(task: task)) {
+                        VStack(alignment: .leading) {
+                            Text(task.title)
+                                .font(.headline)
+                            Text(task.category)
+                                .font(.caption)
+                                .foregroundStyle(Color.secondary)
+                        }
                     }
-                    
                 }
             }.navigationTitle("Tasks")
         }
---
+++ Lesson_Tasks_2026/TaskDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  TaskDetailView.swift
+//  Lesson_Tasks_2026
+//
+//  Created by Stanley Sim on 21/1/26.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    let task: Task
+    
+    var body: some View {
+        Text(task.title)
+            .font(.largeTitle)
+        Text(task.category)
+            .font(.title)
+        Spacer()
+    }
+}
+
+#Preview {
+    TaskDetailView(task: Task(title:"Maths HW", category: "School"))
+}
+
