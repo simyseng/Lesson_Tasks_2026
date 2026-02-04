8f6effbe7a7c8505bbb702a6ae4d06a4f8c97ff7
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-02-04 12:25:03 +0800

    Update TaskDetailView.swift

--- Lesson_Tasks_2026/TaskDetailView.swift
+++ Lesson_Tasks_2026/TaskDetailView.swift
@@ -28,11 +28,6 @@ struct TaskDetailView: View {
                 Toggle("Completed", isOn: $task.isCompleted)
             }
         }
-        
-        
-            
-            
-        
     }
 }
 
