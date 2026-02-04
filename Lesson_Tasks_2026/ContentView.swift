19c3961f91333ace8118a80e04646d44ec698fe0
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-02-04 11:26:41 +0800

    Update ContentView.swift

--- Lesson_Tasks_2026/ContentView.swift
+++ Lesson_Tasks_2026/ContentView.swift
@@ -19,13 +19,17 @@ struct ContentView: View {
             List {
                 ForEach(tasks) { task in
                     NavigationLink (destination: TaskDetailView(task: task)) {
-                        VStack(alignment: .leading) {
-                            Text(task.name)
-                                .font(.headline)
-                            Text(task.category)
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
+                        HStack {
+                            Text(task.isCompleted ? "✅" : "⭕️")
+                            VStack(alignment: .leading) {
+                                Text(task.name)
+                                    .font(.headline)
+                                Text("\(task.category) • Due \(task.dueDate.formatted(date: .abbreviated, time: .omitted)) ")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
                         }
+                        
                     }
                 }
             }.navigationTitle("Tasks")
