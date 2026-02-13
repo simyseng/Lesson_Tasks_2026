3c5651be43453ff4d656dc97b1dfbb5c2bfb9a47
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-02-13 12:21:33 +0800

    Commit

--- Lesson_Tasks_2026/ContentView.swift
+++ Lesson_Tasks_2026/ContentView.swift
@@ -19,16 +19,7 @@ struct ContentView: View {
                 NavigationLink {
                     TaskDetailView(task: $task)
                 } label: {
-                    HStack {
-                        Text(task.isCompleted ? "✅" : "⭕️")
-                        VStack(alignment: .leading) {
-                            Text(task.title)
-                                .font(.headline)
-                            Text("\(task.category) • Due \(task.dueDate.formatted(date: .abbreviated, time: .omitted)) ")
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
-                        }
-                    }
+                    TaskRow(task: task)
                 }
                 
             }.navigationTitle("Tasks")
---
+++ Lesson_Tasks_2026/TaskRow.swift
@@ -0,0 +1,36 @@
+//
+//  TaskRow.swift
+//  Lesson_Tasks_2026
+//
+//  Created by Stanley Sim on 13/2/26.
+//
+
+import SwiftUI
+
+struct TaskRow: View {
+    var task: Task
+
+    var body: some View {
+        HStack {
+            Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "circle")
+                .font(.title3)
+                .foregroundStyle(task.isCompleted ? .green : .gray)
+                .onTapGesture {
+                    task.isCompleted.toggle()
+                }
+            VStack(alignment: .leading) {
+                Text(task.title)
+                    .font(.headline)
+                    .strikethrough(task.isCompleted)
+                    .foregroundStyle(task.isCompleted ? .gray : .primary)
+                Text("\(task.category) • Due \(task.dueDate.formatted(date: .abbreviated, time: .omitted)) ")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+        }
+    }
+}
+
+#Preview {
+    TaskRow(task: Task(title:"Maths Homework", category:"School", dueDate: Date(), isCompleted: false))
+}
