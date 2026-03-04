98db71e2ba2198fe1cf4c01c710c1301be8b3af4
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2026-03-04 11:06:13 +0800

    Commit

--- Lesson_Tasks_2026/Task.swift
+++ Lesson_Tasks_2026/Task.swift
@@ -7,12 +7,24 @@
 
 import Foundation
 
-struct Task: Identifiable {
-    let id = UUID()
+struct Task: Identifiable, Codable, Equatable {
+    let id: UUID
     var name: String
     var category: String
     var dueDate: Date
     var isCompleted: Bool
+    
+    init(id: UUID = UUID(),
+         name: String,
+         category: String,
+         dueDate: Date,
+         isCompleted: Bool) {
+        self.id = id
+        self.name = name
+        self.category = category
+        self.dueDate = dueDate
+        self.isCompleted = isCompleted
+    }
     
     static func newDefaultTask(number: Int) -> Task {
         Task(name: "New Task \(number)",
